cea21511bb931a2511058bcd31f470ae4abb2188
Daniel Robledo <daniel.canorobledo@gmail.com>
2025-04-08 16:27:25 +0200

    [DEVOPS] Allowed ingress host on vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
+    allowedHosts: ['dev.kiwibellako.com', 'localhost', '127.0.0.1'],
     watch: {
       usePolling: true,
     }
