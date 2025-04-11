91f49bdf044804305d7b67a049c0d4724b09b2d6
Daniel Robledo <daniel.canorobledo@gmail.com>
2025-04-11 16:47:34 +0200

    [DEVOPS] Included deployment rollout in CI/CD

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import HelloCard from "./components/HelloCard";
 const App: React.FC = () => {
     return (
         <div className="App">
-            <h1>Front-End Message Display</h1>
+            <h1>Message Display</h1>
             <HelloCard id={1} />
         </div>
     );
