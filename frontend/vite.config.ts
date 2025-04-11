b47bff28a865f314bc1445a4ca278653cf9f8a1f
Daniel Robledo <daniel.canorobledo@gmail.com>
2025-04-11 13:16:45 +0200

    [DEVOPS] Configured allowedhosts on VITE

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: '0.0.0.0',
-    allowedHosts: true,
+    allowedHosts: ["dev.kiwibellako.com", "localhost", "127.0.0.1"],
     watch: {
       usePolling: true,
     }
