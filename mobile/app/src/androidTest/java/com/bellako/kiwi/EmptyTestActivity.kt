46485d55ea0d31e50cba195c6d6e6243716204fc
Daniel Robledo <daniel.canorobledo@gmail.com>
2025-04-28 12:13:46 +0200

    [MOBILE] Removed all "Hello World!" code

--- mobile/app/src/androidTest/java/com/bellako/kiwi/EmptyTestActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.bellako.kiwi.test
-
-import dagger.hilt.android.AndroidEntryPoint
-import androidx.activity.ComponentActivity
-
-@AndroidEntryPoint
-class EmptyTestActivity : ComponentActivity();
--- mobile/app/src/main/java/com/bellako/kiwi/network/HealthApi.kt
+++ mobile/app/src/main/java/com/bellako/kiwi/network/HealthApi.kt
@@ -2,8 +2,10 @@ package com.bellako.kiwi.network
 
 import com.bellako.kiwi.BuildConfig
 import okhttp3.OkHttpClient
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 import java.util.concurrent.TimeUnit
 
 private fun createShortTimeoutOkHttpClient(): OkHttpClient {
@@ -14,11 +16,16 @@ private fun createShortTimeoutOkHttpClient(): OkHttpClient {
         .build()
 }
 
-fun createHealthApiService(): ApiService {
+interface HealthApiService {
+    @GET("actuator/health")
+    suspend fun ping(): Response<Unit>
+}
+
+fun createHealthApiService(): HealthApiService {
     return Retrofit.Builder()
         .baseUrl(BuildConfig.MOBILE_API_URL)
         .client(createShortTimeoutOkHttpClient())
         .addConverterFactory(GsonConverterFactory.create())
         .build()
-        .create(ApiService::class.java)
+        .create(HealthApiService::class.java)
 }
