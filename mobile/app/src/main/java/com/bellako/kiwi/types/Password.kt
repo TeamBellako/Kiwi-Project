34d9dfdeb4374607a7354036e3e4996b6e9e18ab
Daniel Robledo <daniel.canorobledo@gmail.com>
2025-07-22 09:46:51 +0200

    [MOBILE] Reduced password validation for better UX

--- mobile/app/src/main/java/com/bellako/kiwi/types/Password.kt
+++ mobile/app/src/main/java/com/bellako/kiwi/types/Password.kt
@@ -3,12 +3,11 @@ package com.bellako.kiwi.types
 @JvmInline
 value class Password private constructor(val value: String) {
     companion object {
-        private val PASSWORD_REGEX_FULL = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")
+        private val PASSWORD_REGEX_FULL = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d@\$!%*?&]{8,}\$")
         private val PASSWORD_REGEX_LENGTH = Regex("^.{8,}$")
         private val PASSWORD_REGEX_UPPER = Regex(".*[A-Z].*")
         private val PASSWORD_REGEX_LOWER = Regex(".*[a-z].*")
         private val PASSWORD_REGEX_NUMBER = Regex(".*\\d.*")
-        private val PASSWORD_REGEX_SPECIAL = Regex(".*[@$!%*?&].*")
 
         fun isValid(password: String): Boolean {
             return PASSWORD_REGEX_FULL.matches(password)
@@ -23,7 +22,6 @@ value class Password private constructor(val value: String) {
                     else if (!PASSWORD_REGEX_UPPER.matches(password)) "Password should include at least one uppercase letter"
                     else if (!PASSWORD_REGEX_LOWER.matches(password)) "Password should include at least one lowercase letter"
                     else if (!PASSWORD_REGEX_NUMBER.matches(password)) "Password should contain at least one number"
-                    else if (!PASSWORD_REGEX_SPECIAL.matches(password)) "Password should contain at least one special character"
                     else "Invalid password"
                 ))
             }
