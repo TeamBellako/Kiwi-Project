9e4ef3b818361e5e822bf5c0f96d02f96f5b7908
Nacho Martínez <44577104+NachoMC@users.noreply.github.com>
2025-07-28 20:12:31 +0200

    [MOBILE] LoadingModal preview

--- mobile/app/src/main/java/com/bellako/kiwi/ui/modals/LoadingModal.kt
+++ mobile/app/src/main/java/com/bellako/kiwi/ui/modals/LoadingModal.kt
@@ -1,32 +1,61 @@
 package com.bellako.kiwi.ui.modals
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bellako.kiwi.services.common.CommonTestTags
 import com.bellako.kiwi.ui.theme.KiwiTheme
+import com.bellako.kiwi.ui.theme.getResponsiveRelativeSize
 
 @Composable
 fun LoadingModal(
     color: Color = MaterialTheme.colorScheme.secondary,
     trackColor: Color = Color.Transparent
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .testTag(CommonTestTags.LOADING_MODAL),
-        contentAlignment = Alignment.Center
-    ) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Box(modifier = Modifier.size(getResponsiveRelativeSize(50.dp))) {
+            LoadingIcon(color, trackColor)
+        }
+    }
+}
+
+@Composable
+fun LoadingIcon(
+    color: Color = MaterialTheme.colorScheme.secondary,
+    trackColor: Color = Color.Transparent
+) {
+    val isPreview = LocalInspectionMode.current
+    if (isPreview) {
+        // Indicators not shown in preview, show as static circle instead
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            drawCircle(
+                color = color,
+                center = Offset(size.width / 2f, size.height / 2f),
+                radius = (size.minDimension - 11f) / 2f,
+                style = Stroke(width = 11f)
+            )
+        }
+    } else {
+        // Actual indicator in runtime
         CircularProgressIndicator(
             color = color,
-            trackColor = trackColor
+            trackColor = trackColor,
+            modifier = Modifier
+                .fillMaxSize()
+                .testTag(CommonTestTags.LOADING_MODAL)
         )
     }
 }
