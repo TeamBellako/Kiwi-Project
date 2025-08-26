945bb6557bcaa30802d442c888a75fbb0204a981
Nacho Martínez <44577104+NachoMC@users.noreply.github.com>
2025-08-26 19:57:46 +0200

    fix settings id

--- backend/src/test/resources/TestSetUp.sql
+++ backend/src/test/resources/TestSetUp.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS users (
 
 -- Create settings table
 CREATE TABLE IF NOT EXISTS settings (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     sound_volume FLOAT NOT NULL CHECK (sound_volume >= 0 AND sound_volume <= 1),
     music_volume FLOAT NOT NULL CHECK (music_volume >= 0 AND music_volume <= 1),
 
