f2c02692c03726f243fd66fe6463fb22f8dd19b3
Nacho Martínez <44577104+NachoMC@users.noreply.github.com>
2025-09-09 16:24:13 +0200

    Update Permissions.kt

--- mobile/app/src/main/java/com/bellako/kiwi/common/utils/Permissions.kt
+++ mobile/app/src/main/java/com/bellako/kiwi/common/utils/Permissions.kt
@@ -6,13 +6,16 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 
 @RequiresApi(Build.VERSION_CODES.Q)
-fun hasUsageStatsPermission(context: Context): Boolean {
-    val appOps = context.getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
-    val mode =
-        appOps.unsafeCheckOpNoThrow(
-            AppOpsManager.OPSTR_GET_USAGE_STATS,
-            android.os.Process.myUid(),
-            context.packageName,
-        )
-    return mode == AppOpsManager.MODE_ALLOWED
-}
+fun hasUsageStatsPermission(context: Context): Boolean =
+    try {
+        val appOps = context.getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+        val mode =
+            appOps.unsafeCheckOpNoThrow(
+                AppOpsManager.OPSTR_GET_USAGE_STATS,
+                android.os.Process.myUid(),
+                context.packageName,
+            )
+        mode == AppOpsManager.MODE_ALLOWED
+    } catch (_: NullPointerException) {
+        false
+    }
