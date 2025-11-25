aa4a54c04a53fe4447195d94df411d1a88310aec
Raquel <rakelmasg@gmail.com>
2025-11-25 13:07:57 +0100

    Added rate limit filter to RateLimitFilterTest

--- backend/src/test/java/com/kiwi/security/RateLimitFilterTests.java
+++ backend/src/test/java/com/kiwi/security/RateLimitFilterTests.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Import(RateLimitFilter.class)
 @RunWith(SpringRunner.class)
 @WebMvcTest(PingController.class)
 @AutoConfigureMockMvc
