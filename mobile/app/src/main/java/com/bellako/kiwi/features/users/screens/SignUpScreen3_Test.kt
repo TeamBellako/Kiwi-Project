868061340831ee02f40de165e4751b665ae597ab
Daniel Robledo <daniel.canorobledo@gmail.com>
2026-01-12 15:41:16 +0100

    [MOBILE] Implemented build modal

--- mobile/app/src/main/java/com/bellako/kiwi/features/users/screens/SignUpScreen3_Test.kt
+++ mobile/app/src/main/java/com/bellako/kiwi/features/users/screens/SignUpScreen3_Test.kt
@@ -2,7 +2,6 @@ package com.bellako.kiwi.features.users.screens
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
