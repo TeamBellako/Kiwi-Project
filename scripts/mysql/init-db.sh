a2658177bc9b172507dd5becaf4aeae12be620f1
davidmubernal <davidmubernal@gmail.com>
2026-01-23 11:54:03 +0100

    DB

--- scripts/mysql/init-db.sh
+++ scripts/mysql/init-db.sh
@@ -233,6 +233,70 @@ CREATE TABLE IF NOT EXISTS user_subquest_status (
     FOREIGN KEY (subquest_id) REFERENCES subquests(id)
 );
 
+-- Create conversation table
+CREATE TABLE IF NOT EXISTS conversations (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    type ENUM('FULL', 'SMALL') NOT NULL,
+    sprite INT NOT NULL,
+    expresion INT NOT NULL,
+    dialog TEXT NOT NULL,
+    background INT NOT NULL,
+    fx INT,
+    delay_start_ms INT,
+    delay_end_ms INT,
+    next_event ENUM('CONVERSATION', 'BATTLE', 'END') NOT NULL,
+    event_id BIGINT,
+
+    FOREIGN KEY (sprite) REFERENCES sprites(id),
+    FOREIGN KEY (expresion) REFERENCES expressions(id),
+    FOREIGN KEY (background) REFERENCES backgrounds(id)
+);
+
+-- Create conversation_options table
+CREATE TABLE IF NOT EXISTS conversation_options (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    conversation_id BIGINT NOT NULL,
+    text VARCHAR(500) NOT NULL,
+    next_event_id BIGINT NOT NULL,
+    cost INT 
+);
+
+-- Create user_conversation_selection table
+CREATE TABLE IF NOT EXISTS user_conversation_selection (
+    user_id BIGINT NOT NULL,
+    conversation_id BIGINT NOT NULL,
+    option_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, conversation_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (conversation_id) REFERENCES conversations(id),
+    FOREIGN KEY (option_id) REFERENCES conversation_options(id
+);
+
+-- Create sprites DB
+CREATE TABLE IF NOT EXISTS sprites (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+);
+
+-- Create expressions DB
+CREATE TABLE IF NOT EXISTS expressions (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+);
+
+-- Create backgrounds DB
+CREATE TABLE IF NOT EXISTS backgrounds (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+);
+
+-- Create fx DB
+CREATE TABLE IF NOT EXISTS fx (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+);
+
 -- Trigger to DELETE ON CASCADE user_quest_status table
 DELIMITER $$
 
