0ccc542e62220f12d223b8bef2aad5004bd0732e
Daniel Robledo <daniel.canorobledo@gmail.com>
2026-03-05 11:26:29 +0100

    [MOBILE] Added event names and general payload

--- mobile/app/src/main/java/com/bellako/kiwi/common/services/eventbus/Events.kt
+++ mobile/app/src/main/java/com/bellako/kiwi/common/services/eventbus/Events.kt
@@ -6,6 +6,14 @@ import com.bellako.kiwi.features.map.data.MapInfo
 enum class EventType {
     SWITCH_MAP,
     CHANGE_DASHBOARD_LAYOUT,
+    START_CNV,
+    START_COMBAT,
+    START_QUEST,
+    COMPLETE_QUEST,
+    COMPLETE_GOAL,
+    GAIN_SKILL,
+    THROW_SKILL,
+    UNLOCK_NODE,
 }
 
 sealed class EventPayload {
@@ -15,5 +23,9 @@ sealed class EventPayload {
 
     data class ChangeDashboardLayoutPayload(
         val newLayout: DashboardLayout,
+    ) : EventPayload()
+
+    data class EntityIdPayload(
+        val targetEntityId: Int,
     ) : EventPayload()
 }
