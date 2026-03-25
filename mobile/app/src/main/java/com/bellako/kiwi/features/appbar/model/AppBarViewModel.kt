666b3b11a6ff1938cda14139e6524586a3222021
Daniel Cano Robledo <daniel.canorobledo@gmail.com>
2026-03-25 15:51:10 +0100

    [MOBILE] Added events for notifying changes in both goals and quests and show the app bar's update badge (#102)

--- mobile/app/src/main/java/com/bellako/kiwi/features/appbar/model/AppBarViewModel.kt
+++ mobile/app/src/main/java/com/bellako/kiwi/features/appbar/model/AppBarViewModel.kt
@@ -3,15 +3,22 @@ package com.bellako.kiwi.features.appbar.model
 import com.bellako.kiwi.R
 import com.bellako.kiwi.common.data.ScreenRoutes
 import com.bellako.kiwi.common.model.BaseViewModel
+import com.bellako.kiwi.common.services.eventbus.EventType
+import com.bellako.kiwi.common.services.eventbus.listenToEvent
 import com.bellako.kiwi.features.appbar.data.AppBarItem
 import com.bellako.kiwi.features.appbar.data.AppBarState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@OptIn(DelicateCoroutinesApi::class)
 @HiltViewModel
 class AppBarViewModel
     @Inject
@@ -20,6 +27,20 @@ class AppBarViewModel
         IAppBarViewModel {
         private val _currentRoute = MutableStateFlow<String?>(null)
         val currentRoute: StateFlow<String?> = _currentRoute.asStateFlow()
+
+        init {
+            GlobalScope.launch(Dispatchers.Main) {
+                listenToEvent(EventType.QUESTS_UPDATED) { eventPayload ->
+                    onNewContent(ScreenRoutes.OBJECTIVES)
+                }
+            }
+
+            GlobalScope.launch(Dispatchers.Main) {
+                listenToEvent(EventType.DAILY_GOALS_UPDATED) { eventPayload ->
+                    onNewContent(ScreenRoutes.OBJECTIVES)
+                }
+            }
+        }
 
         private val _state =
             MutableStateFlow(
