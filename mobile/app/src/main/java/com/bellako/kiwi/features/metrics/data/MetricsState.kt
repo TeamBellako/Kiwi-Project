08b6afc7987824e9cebea662478417fb76b7e7fd
davidmubernal <davidmubernal@gmail.com>
2026-05-14 16:25:51 +0200

    [Mobile] Fix MetricState

--- mobile/app/src/main/java/com/bellako/kiwi/features/metrics/data/MetricsState.kt
+++ mobile/app/src/main/java/com/bellako/kiwi/features/metrics/data/MetricsState.kt
@@ -7,10 +7,21 @@ data class MetricsState(
     val maxBadTimeSeconds: Int,
     val currentBadTimeSeconds: Int,
 ) {
-    fun getAppUsageProgress(): Float =
-        if (maxGoodTimeSeconds > 0) {
-            (currentGoodTimeSeconds.toFloat() / maxGoodTimeSeconds.toFloat()).coerceIn(0f, 1f)
-        } else {
-            0f
-        }
+    fun getAppUsageProgress(): Float {
+        val goodTimeProgress =
+            if (maxGoodTimeSeconds > 0) {
+                (currentGoodTimeSeconds.toFloat() / maxGoodTimeSeconds.toFloat()).coerceIn(0f, 1f)
+            } else {
+                0f
+            }
+
+        val badTimeProgress =
+            if (maxBadTimeSeconds > 0) {
+                (1f - currentBadTimeSeconds.toFloat() / maxBadTimeSeconds.toFloat()).coerceIn(0f, 1f)
+            } else {
+                0f
+            }
+
+        return (goodTimeProgress + badTimeProgress) / 2f
+    }
 }
